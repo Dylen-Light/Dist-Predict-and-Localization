75b6b98773f0191ebe36b60c55d40d12236f9277
Dylen_Light <1610854495@qq.com>
2024-05-15 02:21:56 +0800

    flask框架写了点小请求

--- application.py
+++ application.py
@@ -1,23 +1,67 @@
+import json
+import threading
+
 import joblib
 import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import serial
 from scipy.optimize import least_squares
+from flask import Flask
 import csv
 
-def exec_predict(rssi, rtt, csi):
-    print("executing dest prediction with param:{ RSSI:", rssi, ", RTT:", rtt, ", CSI:", csi, "}.")
-    raw_data = np.array([[rssi, rtt, csi]])
-    raw_data = MinMaxScaler.transform(raw_data.reshape(3, 1))
-    raw_data = raw_data.reshape(1, 1, 3)
 
-    dst_predict = model.predict(raw_data)
-    dst_predict = dst_predict.reshape(len(dst_predict), 1)
-    dst_predict = MinMaxScaler.inverse_transform(dst_predict)
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj,APInformation):
+            d = {'__class__': obj.__class__.__name__, '__module__': obj.__module__}
+            d.update(obj.__dict__)
+            return d
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+class APInformation:
+    def __init__(self, apid, pos_x, pos_y, dist, is_valid, is_los):
+        self.apid = apid
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dist = dist
+        self.is_valid = is_valid
+        self.is_los = is_los
+
+
+def exec_predict(rssi, rtt, csi):
+    # print("executing dest prediction with param:{ RSSI:", rssi, ", RTT:", rtt, ", CSI:", csi, "}.")
+    raw_data = np.array([csi])
+    raw_data = np.insert(raw_data, 0, rtt)
+    raw_data = np.insert(raw_data, 0, rssi)
+    # print(raw_data)
+    feature_len = 2 + 128
+    raw_data = MinMaxScaler.transform(raw_data.reshape(feature_len, 1))
+    raw_data = raw_data.reshape(1, 1, feature_len)
+    # los_nlos = classify_model.predict(raw_data)
+    # los_nlos = los_nlos.reshape(len(los_nlos), 1)
+    # los_nlos = MinMaxScaler.inverse_transform(los_nlos)
+    los_nlos = 1  # 调试用
+    if los_nlos == 1:
+        dst_predict = los_pre_model.predict(raw_data)
+        dst_predict = dst_predict.reshape(len(dst_predict), 1)
+        dst_predict = MinMaxScaler.inverse_transform(dst_predict)
+    else:
+        dst_predict = nlos_pre_model.predict(raw_data)
+        dst_predict = dst_predict.reshape(len(dst_predict), 1)
+        dst_predict = MinMaxScaler.inverse_transform(dst_predict)
+
     # dst_predict = dst_predict.reshape(len(dst_predict), 1)
     print(dst_predict[0][0])
-    return dst_predict[0][0]
+    return dst_predict[0][0], los_nlos
 
 
 class vec3d:
@@ -71,51 +115,19 @@ def get_distance(anchor_positions, tag_position):
     return distances
 
 
-# if __name__ == '__main__':
-#     # 基站坐标
-#     anchor_positions = [
-#         vec3d(0, 0, 0.5),
-#         vec3d(6, 0, 0.5),
-#         vec3d(6, 6, 0.5),
-#         vec3d(0, 6, 2.5),
-#         vec3d(12, 6, 1.5),
-#         vec3d(18, 12, 0.5),
-#     ]
-#
-#     # 通过指定标签坐标，计算与各基站的距离
-#     # tag_position = vec3d(3, 3, 1)
-#     # distances = get_distance(anchor_positions, tag_position)
-#     # print(distances)
-#
-#     # 通过指定距离，计算标签坐标
-#     distances = np.array([4.27200187, 4.27200187, 4.27200187, 4.5, 9.5, 17.5])
-#     estimated_position = trilateration(anchor_positions, distances)
-#     estimated_position = np.round(estimated_position, 3)
-#     print("Estimated Position:", estimated_position)
-#     # 验证距离以检查答案是否正确
-#     for i in range(len(anchor_positions)):
-#         print(
-#             calculate_distance(
-#                 vec3d(estimated_position[0], estimated_position[1],
-#                       estimated_position[2]), anchor_positions[i]))
-
-if __name__ == '__main__':
-    print("loading model...")
-    model = load_model("mlp_model.h5")
-    MinMaxScaler = joblib.load('mmScaler')
+def info_collector():
     ser = serial.Serial('COM12', 115200)
-    #设置串口
+    # 设置串口
     distance = 300
     # 设置实际距离，单位mm
     isLoS = 1
-    # 设置是否是LoS环境，1为是0为否
+    # 设置是否是LoS环境，1为是0为否，当然这里是收集数据用的
     with open('FTM_Predict.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
+        # writer = csv.writer(file)
         # writer.writerow(["SN", "Name", "Contribution"])
 
         if ser.is_open:
             print("serial has opened.")
-
             # 构造参数数据结构
             anchor_positions = []
             dist = []
@@ -123,28 +135,79 @@ if __name__ == '__main__':
                 data = ser.readline().decode('UTF-8')
                 if "predicting" in data:
                     data = "".join(filter(lambda s: s in '0123456789,.', data))
-                    print(data)
+                    # print(data)
                     params = data.split(',')
                     # print(params)
                     # params.append(isLoS)
                     params.append(distance)
-                    writer.writerow(params)
-                    # pred_dist = exec_predict(params[0], params[1], 5)
-                    # if int(params[2]) >= len(anchor_positions):
-                    #     anchor_positions.append(vec3d(float(params[3]),float(params[4]),0))
-                    #     dist.append(pred_dist)
-                    # else :
-                    #     #一次循环结束
-                    #     distances = np.array(dist)
-                    #     estimated_position = trilateration(anchor_positions, distances)
-                    #     estimated_position = np.round(estimated_position, 3)
-                    #     print("Estimated Position:", estimated_position)
-                    #     anchor_positions.clear()
-                    #     dist.clear()
+                    # writer.writerow(params)
+                    apid = params[0]
+                    csi = params[5:-1]
+                    # print(csi)
+
+                    pred_dist, los_nlos = exec_predict(params[3], params[4], csi)
+
+                    current_ap = APInformation(apid, params[1], params[2], pred_dist, 1, los_nlos)
+                    current_ap_ids.append(apid)
+
+                    print("current ap: " + apid)
+                    if len(ap_info) == 0:
+                        ap_info.append(current_ap)
+                    else:
+                        exist = 0
+                        for i in range(len(ap_info)):
+                            if ap_info[i].apid == apid:
+                                # ap_info.append(current_ap)
+                                ap_info[i].is_valid = 1
+                                exist = 1
+                                break
+                        if exist == 0:
+                            ap_info.append(current_ap)
+
+                    if int(apid) >= len(anchor_positions):
+                        anchor_positions.append(vec3d(float(params[1]), float(params[2]), 0))
+                        dist.append(pred_dist)
+
+                    else:
+                        # 一次循环结束
+
+                        for i in range(len(ap_info)):
+                            if not current_ap_ids.__contains__(ap_info[i].apid):
+                                ap_info[i].is_valid = 0
+                        current_ap_ids.clear()
+                        for info in ap_info:
+                            print("ap: " + str(info.apid) + "isValid: " + str(info.is_valid))
+
+                        distances = np.array(dist)
+                        estimated_position = trilateration(anchor_positions, distances)
+                        estimated_position = np.round(estimated_position, 3)
+                        print("Estimated Position:", estimated_position)
+                        anchor_positions.clear()
+                        dist.clear()
 
 
+app = Flask(__name__)
 
 
+@app.route("/")
+def hello_world():
+    return "<p>hello world</p>"
 
 
+@app.route("/ap")
+def get_ap_info():
+    json_data = json.dumps(ap_info, cls=MyEncoder)
+    return json_data
 
+
+if __name__ == '__main__':
+    ap_info = []
+    current_ap_ids = []
+    print("loading model...")
+    los_pre_model = load_model("mlp_model.h5")
+    nlos_pre_model = load_model("mlp_model.h5")
+    classify_model = load_model("mlp_model.h5")
+    MinMaxScaler = joblib.load('mmScaler')
+    info_collector_thread = threading.Thread(target=info_collector)
+    info_collector_thread.start()
+    app.run()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#原始数据
+# 原始数据
 import joblib
 import matplotlib
 import numpy as np
@@ -9,7 +9,7 @@ from tensorflow.python.layers.core import Dropout
 import tensorflow
 import sklearn.metrics
 from IPython.core.display import SVG
-from keras.layers import LSTM, Dense,Activation, Bidirectional
+from keras.layers import LSTM, Dense, Activation, Bidirectional
 from keras.losses import mean_squared_error
 from keras.models import Sequential
 from keras.utils import model_to_dot, plot_model
@@ -21,68 +21,69 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from keras import regularizers
 from sklearn.preprocessing import MinMaxScaler
-#from statsmodels.tsa.seasonal import seasonal_decompose
+# from statsmodels.tsa.seasonal import seasonal_decompose
 import tensorflow as tf
 import seaborn as sns
 
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 matplotlib.rcParams['font.sans-serif'] = ['SimHei']
 matplotlib.rcParams['axes.unicode_minus'] = False
 
-data=pd.read_excel('kpoeh-xvq4s.xls')
+data = pd.read_excel('FTM_Predict.xls')
 data = DataFrame(data, dtype=float)
-data=data.iloc[:,:]
-train_Standard = data.iloc[:1800,:]
-test_Standard = data.iloc[1800:,:]
+data = data.iloc[:, :]
+train_Standard = data.iloc[:1800, :]
+test_Standard = data.iloc[1800:, :]
 # train_X,train_y = train_Standard.iloc[:,:-1],train_Standard.iloc[:,-1]  #除最后一列，仅最后一列
 # test_X,test_y = test_Standard.iloc[:,:-1],test_Standard.iloc[:,-1]
 
-train_X,train_y = train_Standard.iloc[:,5:-1],train_Standard.iloc[:,-1]
-test_X,test_y = test_Standard.iloc[:,5:-1],test_Standard.iloc[:,-1]
+train_X, train_y = train_Standard.iloc[:, 3:-1], train_Standard.iloc[:, -1]
+test_X, test_y = test_Standard.iloc[:, 3:-1], test_Standard.iloc[:, -1]
 
-train_X, test_X=train_X.values, test_X.values
-train_y, test_y=train_y.values, test_y.values
-print(train_X.shape,train_y.shape)        # (1500,3)   (1500)
-print(test_X.shape,test_y.shape)          # (499,3)    (499)
+train_X, test_X = train_X.values, test_X.values
+train_y, test_y = train_y.values, test_y.values
+print(train_X.shape, train_y.shape)  # (1500,3)   (1500)
+print(test_X.shape, test_y.shape)  # (499,3)    (499)
 print("1.test_X\n")
 print(test_X)
-#归一化
+# 归一化
 MinMaxScaler = MinMaxScaler()
-train_X=MinMaxScaler.fit_transform(train_X)
-train_y = train_y.reshape(train_y.shape[0],1)
-train_y=MinMaxScaler.fit_transform(train_y)
+train_X = MinMaxScaler.fit_transform(train_X)
+train_y = train_y.reshape(train_y.shape[0], 1)
+train_y = MinMaxScaler.fit_transform(train_y)
 
-test_X=MinMaxScaler.fit_transform(test_X)
-test_y = test_y.reshape(test_y.shape[0],1)
-test_y=MinMaxScaler.fit_transform(test_y)
+test_X = MinMaxScaler.fit_transform(test_X)
+test_y = test_y.reshape(test_y.shape[0], 1)
+test_y = MinMaxScaler.fit_transform(test_y)
 print("2.test_X\n")
 print(test_X)
 print(test_X.shape)
 # reshape input to be 3D [samples,timeseps,features]
-train_X = train_X.reshape((train_X.shape[0],1,train_X.shape[1]))
-test_X = test_X.reshape((test_X.shape[0],1,test_X.shape[1]))
-print(train_X.shape,train_y.shape,test_X.shape,test_y.shape)  #(145, 1, 22) (145,) (38, 1, 22) (38,)
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)  # (145, 1, 22) (145,) (38, 1, 22) (38,)
 print("3.test_X\n")
 print(test_X)
 
 # 定义模型
 model = Sequential()
 model.add(Dense(32, input_shape=(train_X.shape[1], train_X.shape[2]),
-                #activity_regularizer=regularizers.l1(0.01),
-                #kernel_regularizer=regularizers.l2(0.01),
+                # activity_regularizer=regularizers.l1(0.01),
+                # kernel_regularizer=regularizers.l2(0.01),
                 activation='relu'
                 )
           )
-#model.add(Dropout(0.5))
-#model.add(Dense(32, activation='relu'))
-#model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dropout(0.5))
 model.add(Dense(1,
                 activation='sigmoid'
                 ))
-model.compile(loss='binary_crossentropy',optimizer='rmsprop')                                   # ,metrics=['accuracy']
+model.compile(loss='binary_crossentropy', optimizer='rmsprop')  # ,metrics=['accuracy']
 # model.compile(loss='binary_crossentropy',optimizer='rmsprop')
-#score = model.evaluate(test_X, test_y, batch_size=128)
+# score = model.evaluate(test_X, test_y, batch_size=128)
 
 # 拟合模型
 batchsize = 10
@@ -92,12 +93,12 @@ model.save('mlp_model.h5')
 # print(history.history.keys())
 # plot history
 
-#plt.subplot(len(groups),1,i)
+# plt.subplot(len(groups),1,i)
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='test')
 plt.xlabel('epoch')
 plt.ylabel('loss')
-plt.title('batch_size = %d' %batchsize)
+plt.title('batch_size = %d' % batchsize)
 plt.legend()
 plt.show()
 
@@ -115,29 +116,27 @@ test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 # invert scaling for actual
 train_y = train_y.reshape((len(train_y), 1))
 test_y = test_y.reshape((len(test_y), 1))
-yhat = yhat.reshape(len(yhat),1)
-yhat=MinMaxScaler.inverse_transform(yhat)
-train_y=MinMaxScaler.inverse_transform(train_y)
-test_y=MinMaxScaler.inverse_transform(test_y)
+yhat = yhat.reshape(len(yhat), 1)
+yhat = MinMaxScaler.inverse_transform(yhat)
+train_y = MinMaxScaler.inverse_transform(train_y)
+test_y = MinMaxScaler.inverse_transform(test_y)
 yhat = yhat.reshape((len(yhat), 1))
-joblib.dump(MinMaxScaler,'mmScaler')
+joblib.dump(MinMaxScaler, 'mmScaler')
 
 # calculate RMSE
-yhat=yhat.ravel()
-test_y=test_y.ravel()
+yhat = yhat.ravel()
+test_y = test_y.ravel()
 
-mape = np.mean(np.abs((yhat-test_y)/(test_y)))*100
+mape = np.mean(np.abs((yhat - test_y) / (test_y))) * 100
 print('=============mape==============')
-print(mape,'%')
+print(mape, '%')
 
 # 画出真实数据和预测数据的对比曲线图
-print("R2 = ",metrics.r2_score(test_y, yhat)) # R2
-plt.plot(test_y,color = 'red',label = 'true')
-plt.plot(yhat,color = 'blue', label = 'pred')
+print("R2 = ", metrics.r2_score(test_y, yhat))  # R2
+plt.plot(test_y, color='red', label='true')
+plt.plot(yhat, color='blue', label='pred')
 plt.title('Prediction')
 plt.xlabel('Time')
 plt.ylabel('value')
 plt.legend()
 plt.show()
-
-
